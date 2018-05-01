d356383e66562e025393794cc9eca5293600ff02
DShams <Danielshams2014@u.northwestern.edu>
2018-04-30 19:06:18 -0500

    Update Game.py

--- Game.py
+++ Game.py
@@ -3,24 +3,19 @@
 
 import numpy as np
 
-
 # Set up truncated poisson - N is the number of steps this game
 lam = 7;
 s = np.random.poisson(lam,1000);
 s = s[s <= 10];
 N = np.random.choice(s,1);
 
-
-
 #Initialization
 A1_e =  0.0;
 A1_m = np.zeros(6);
 A1_p = np.zeros(3);
-
-A2_e =  0.6;
+A2_e =  0.1;
 A2_m = np.zeros(6);
 A2_p = np.zeros(3);
-
 
 #Game
 for i in range(N[0]):
